044dd870bf48d28b8c2f91699662b681d89a5564
GfCarvalho77 <127401543+GfCarvalho77@users.noreply.github.com>
2024-03-31 16:08:28 -0300

    Bordas das abas e pointer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
-   
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -9,11 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
-            //removeBotaoAtivo();
+            removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 })
+
+function removeBotaoAtivo(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
 
 function escondeTodasAbas(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
