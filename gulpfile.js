2992597806d9f6278b5dc1fe94807a9b8752c894
GfCarvalho77 <127401543+GfCarvalho77@users.noreply.github.com>
2024-04-11 08:50:55 -0300

    cconfigurando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const gulp = require('gulp');//Importado gulp
 const sass = require('gulp-sass')(require('sass')); //Importando Sass
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
+function scripts(){
+    return gulp.src('./src/scripts/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -14,9 +20,10 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 
 exports.watch=function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/main.js', gulp.parallel(scripts))
 }
